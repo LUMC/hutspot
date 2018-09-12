a7373cdd0919d22cf64c6795f02771d8e8a50d67
Sander Bollen <a.h.b.bollen@lumc.nl>
2018-09-12 12:09:50 +0200

    explicitly put output of bc -l in variable. Fixes check of fraction.

--- src/seqtk.sh
+++ src/seqtk.sh
@@ -7,21 +7,22 @@ input_fastq=${2}
 output_fastq=${3}
 max_bases=${4}
 
-if [[ $max_bases -eq 'None' ]]; then
-    ln -s $input_fastq $output_fastq
+if [[ ${max_bases} -eq 'None' ]]; then
+    ln -s ${input_fastq} ${output_fastq}
     exit 0
 fi
 
 if [[ -z $max_bases ]]; then
-    ln -s $input_fastq $output_fastq
+    ln -s ${input_fastq} ${output_fastq}
     exit 0
 fi
 
 bases=$(jq '.bases' $count_json)
 frac=$(jq -n "$max_bases / $bases" | sed -e "s:e:E:g")
 echo $frac
-if (( $(echo "$frac > 1" | bc -l) )); then
-    ln -s $input_fastq $output_fastq
+frac_higher_than_one=$(echo "${frac} > 1" | bc -l)
+if [[ ${frac_higher_than_one} -eq 1 ]]; then
+    ln -s ${input_fastq} ${output_fastq}
 else
-    seqtk sample -s100 $frac $input_fastq | gzip -c > $output_fastq
+    seqtk sample -s100 ${input_fastq} ${frac} | gzip -c > ${output_fastq}
 fi
