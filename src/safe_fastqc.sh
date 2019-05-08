2a5d61bb17d0e47371dc96b52b56fe7276f9ed48
Sander Bollen <a.h.b.bollen@lumc.nl>
2019-05-08 13:57:28 +0200

    use unzip -l instead of unzip -t since busybox does not contain -t

--- src/safe_fastqc.sh
+++ src/safe_fastqc.sh
@@ -26,13 +26,13 @@ odir=${5}
 fastqc --nogroup -o ${odir} ${input_r1} ${input_r2}
 
 if [[ -f ${output_r1} ]]; then
-    unzip -t ${output_r1} || truncate -s0 ${output_r1}
+    unzip -l ${output_r1} || truncate -s0 ${output_r1}
 else
     touch ${output_r1}
 fi
 
 if [[ -f ${output_r2} ]]; then
-    unzip -t ${output_r2} || truncate -s0 ${output_r2}
+    unzip -l ${output_r2} || truncate -s0 ${output_r2}
 else
     touch ${output_r2}
 fi
