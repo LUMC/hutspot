9c319c5cc7927359b4596d3907d1b7550b6b134b
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2020-05-29 08:00:48 +0200

    Add test for raw bases and reads
    Also update pytest code to be compatible with pytest-workflow 1.4.0

--- tests/test_stats_output.py
+++ tests/test_stats_output.py
@@ -5,14 +5,14 @@ import pytest
 import os
 
 
-@pytest.mark.workflow(name='test-integration-no-cluster')
+@pytest.mark.workflow('test-integration-no-cluster')
 def test_stats_file_exists(workflow_dir):
     stats_file = 'stats.tsv'
     full_path = workflow_dir / pathlib.Path(stats_file)
     assert os.path.exists(full_path)
 
 
-@pytest.mark.workflow(name='test-integration-no-cluster')
+@pytest.mark.workflow('test-integration-no-cluster')
 def test_stats_file_name(workflow_dir):
     """ Read in the stats file and do some tests """
     stats_file = 'stats.tsv'
@@ -26,7 +26,7 @@ def test_stats_file_name(workflow_dir):
     assert data['sample_name'] == 'micro'
 
 
-@pytest.mark.workflow(name='test-integration-no-cluster')
+@pytest.mark.workflow('test-integration-no-cluster')
 def test_stats_file_mapped_reads(workflow_dir):
     """ Read in the stats file and do some tests """
     stats_file = 'stats.tsv'
@@ -40,7 +40,7 @@ def test_stats_file_mapped_reads(workflow_dir):
     assert data['mapped_reads'] == '15558'
 
 
-@pytest.mark.workflow(name='test-integration-no-cluster')
+@pytest.mark.workflow('test-integration-no-cluster')
 def test_stats_file_mapped_bases(workflow_dir):
     """ Read in the stats file and do some tests """
     stats_file = 'stats.tsv'
@@ -54,7 +54,7 @@ def test_stats_file_mapped_bases(workflow_dir):
     assert data['mapped_bases'] == '2280294'
 
 
-@pytest.mark.workflow(name='test-integration-no-cluster')
+@pytest.mark.workflow('test-integration-no-cluster')
 def test_stats_file_usable_reads(workflow_dir):
     """ Read in the stats file and do some tests """
     stats_file = 'stats.tsv'
@@ -68,7 +68,7 @@ def test_stats_file_usable_reads(workflow_dir):
     assert data['usable_reads'] == '15515'
 
 
-@pytest.mark.workflow(name='test-integration-no-cluster')
+@pytest.mark.workflow('test-integration-no-cluster')
 def test_stats_file_usable_bases(workflow_dir):
     """ Read in the stats file and do some tests """
     stats_file = 'stats.tsv'
@@ -80,3 +80,31 @@ def test_stats_file_usable_bases(workflow_dir):
     data = dict(zip(header, values))
 
     assert data['usable_bases'] == '2275470'
+
+
+@pytest.mark.workflow('test-integration-no-cluster')
+def test_stats_file_preqc_reads(workflow_dir):
+    """ Read in the stats file and do some tests """
+    stats_file = 'stats.tsv'
+    full_path = workflow_dir / pathlib.Path(stats_file)
+    # Since we only have two lines in the test stats file, this works
+    with open(full_path, 'r') as fin:
+        header = next(fin).strip().split('\t')
+        values = next(fin).strip().split('\t')
+    data = dict(zip(header, values))
+
+    assert data['preqc_reads'] == '15440'
+
+
+@pytest.mark.workflow('test-integration-no-cluster')
+def test_stats_file_preqc_bases(workflow_dir):
+    """ Read in the stats file and do some tests """
+    stats_file = 'stats.tsv'
+    full_path = workflow_dir / pathlib.Path(stats_file)
+    # Since we only have two lines in the test stats file, this works
+    with open(full_path, 'r') as fin:
+        header = next(fin).strip().split('\t')
+        values = next(fin).strip().split('\t')
+    data = dict(zip(header, values))
+
+    assert data['preqc_bases'] == '2276743'
